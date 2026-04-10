a1fe89f825a71252ea97079aecddded0f1fda270
wissem ben slima <wissem.ben.slima@tek-up.de>
2026-04-10 16:39:08 +0100

    operation create

---
+++ fireBaseApi/ApiHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+internal class ApiHandler
+{
+    static readonly HttpClient client = new HttpClient();
+    public string Url { get; set; }
+    public HttpMethod Method { get; set; }
+    public string Body { get; set; }
+
+    public async Task<HttpResponseMessage> SendRequest()
+    {
+        try
+        {
+            HttpRequestMessage request = new HttpRequestMessage(Method, Url);
+            if (Method != HttpMethod.Get || Method != HttpMethod.Delete)
+            {
+                StringContent content = new StringContent(Body, Encoding.UTF8, "application/json");
+                request.Content = content;
+            }
+            HttpResponseMessage response = await client.SendAsync(request);
+            return response;
+
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"request failed: {ex.Message}");
+
+        }
+    }
+}
+
---
+++ fireBaseApi/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        await Create();
+
+
+    }
+    static async Task Create()
+    {
+        var Api = new ApiHandler()
+        {
+            Url = "https://fir-2026-bf25f-default-rtdb.firebaseio.com/Info.json",
+            Method = HttpMethod.Post,
+            Body = """
+            
+            {
+                "Anime":{
+                        "Name":"Dragon Ball Z",
+                         "Protagonist":"Goku"
+
+
+                        }
+            }
+            
+            """
+
+        };
+        var response = await Api.SendRequest();
+        if (response.StatusCode == HttpStatusCode.OK) 
+        {
+            Console.WriteLine("Data  created succefuly");
+
+
+         }
+        else
+            {
+            Console.WriteLine($"Error:{response.StatusCode}");
+
+
+        }
+
+    }
+}
