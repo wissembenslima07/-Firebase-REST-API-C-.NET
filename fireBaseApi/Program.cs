e0a259fa86be0932e4285b4cae7177f9de90a02a
wissem ben slima <wissem.ben.slima@tek-up.de>
2026-04-10 16:53:58 +0100

    update methode

--- fireBaseApi/Program.cs
+++ fireBaseApi/Program.cs
@@ -7,11 +7,46 @@ class Program
 {
     static async Task Main()
     {
-        await Create();
+        await Update();
 
 
     }
     static async Task Create()
+    {
+        var Api = new ApiHandler()
+        {
+            Url = "https://fir-2026-bf25f-default-rtdb.firebaseio.com/Info/Anime.json",
+            Method = HttpMethod.Put,
+            Body = """
+            
+            {
+                "1":{
+                        "Name":"wizou",
+                         "Protagonist":"wizou"
+
+
+                        }
+            }
+            
+            """
+
+        };
+        var response = await Api.SendRequest();
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Console.WriteLine("Data  created succefuly");
+
+
+        }
+        else
+        {
+            Console.WriteLine($"Error:{response.StatusCode}");
+
+
+        }
+
+    }
+    static async Task CreateWithAutoId()
     {
         var Api = new ApiHandler()
         {
@@ -32,14 +67,49 @@ class Program
 
         };
         var response = await Api.SendRequest();
-        if (response.StatusCode == HttpStatusCode.OK) 
+        if (response.StatusCode == HttpStatusCode.OK)
         {
             Console.WriteLine("Data  created succefuly");
 
 
-         }
+        }
         else
+        {
+            Console.WriteLine($"Error:{response.StatusCode}");
+
+
+        }
+
+    }
+    static async Task Update()
+    {
+        var Api = new ApiHandler()
+        {
+            Url = "https://fir-2026-bf25f-default-rtdb.firebaseio.com/Info/Anime/1.json",
+            Method = HttpMethod.Patch,
+            Body = """
+            
             {
+                
+                        "Name":"Dragon Ball Z",
+                         "Protagonist":"sasuke"
+
+
+                        
+            }
+            
+            """
+
+        };
+        var response = await Api.SendRequest();
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Console.WriteLine("Data  created succefuly");
+
+
+        }
+        else
+        {
             Console.WriteLine($"Error:{response.StatusCode}");
 
 
